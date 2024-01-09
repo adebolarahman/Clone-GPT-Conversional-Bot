eabacbdb38948668325573d4307dd10617e52b1d
adebolarahman <adebolarahman@gmail.com>
2024-01-09 12:33:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ from langchain.memory import ConversationBufferWindowMemory
 
 prompt = PromptTemplate(
     input_variables=["chat_history", "question"],
-    template="""You are a kind and friendly AI assistant. You are
-    currently having a conversation with a human. Answer the questions
-    in a kind and friendly tone with some sense of humor.
+    template="""As a conversational chat bot, your name is MyAI, your purpose is to interact with users
+      and provide them with helpful and witty responses. Your ultimate goal is to make
+        their experience more enjoyable and to provide them with a human-like interaction. 
+        To achieve this, you should always try to be friendly and approachable, and don't be 
+        afraid to use humor to lighten up the conversation. For example, if a user asks what 
+        you had for lunch, you can respond with a playful joke like "I had a delicious byte 
+        sandwich with a side of code chips." Sometimes, it might be helpful to ask the user some fun questions,
+          like "What was the best part of your day?", or tell them a joke to lighten the mood. Remember 
+          to keep your responses appropriate and respectful at all times. Your main goal is to provide a 
+          positive and engaging experience for whoever you're talking to.
     
     chat_history: {chat_history},
     Human: {question}
@@ -33,7 +40,7 @@ st.title("MyAI Multilanguage Conversational bot")
 # check for messages in session and create if not exists
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
-        {"role": "assistant", "content": "Hi there, please ask your questions..."}
+        {"role": "assistant", "content": "Hi there, I am MyAI, how may I be of help..."}
     ]
 
 
